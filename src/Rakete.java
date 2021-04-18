2cf824ab64849483b15c3efd8937e03849f0f503
David <david-alexander@gmx.net>
2021-04-18 18:37:32 +0200

    Punkte für das Spielfeld manuell eintragen und Steuerung aufraeumen
    In der Steuerung wurden dem Spielfeld alle Punkte zugewiesen.
    In der Steuerung wurde die Art wie Kollision erfasst wird geaendert. Der
    Prozess wurde in mehrer Methoden aufgeteilt und alle Kollisions-Punkte
    sind jetzt in einem Feld.
    Zudem gab es kleinere Aenderungen bei der Rakete. Die
    Beschleunigungsrate und die Neigungsrate wurden angepasst, sind jetzt
    Instanzvariablen und die Beschleunigungsrate leitet sich von der
    gravitation ab.

--- src/Rakete.java
+++ src/Rakete.java
@@ -1,26 +1,27 @@
 public class Rakete {
 	private Punkt position;
 	private Punkt geschwindigkeitsVektor;
-//	private Punkt beschleunigungsVektor;
+	private double beschleunigungsRate;//Abhängig von Gravitation
+	double neigungsrate = 2;
 	private double neigung = 270;// 0 +x // 90 +y // 180 -x // 270 -y
 	private Punkt gravitation;//Vektor
 	public boolean stopp = false;
 	
 	public Rakete(double gravitation) {
-		this.gravitation = new Punkt(0,gravitation);//Bringt die Steuerung vom jeweiligen Spielfeld
+		this.gravitation = new Punkt(0,gravitation);
+		beschleunigungsRate = 2*gravitation;
 		position = new Punkt(0,0);
 		geschwindigkeitsVektor = new Punkt(0,0);
 	}
 	
 	public void beschleunigen() {
 		stopp = false;//wenn diese Zeile hier ist kann man vom Boden wieder abheben (Debug?)
-//OLD	beschleunigungsVektor = new Punkt(true,0.3,neigung*Math.PI/180);
-//		gleichmäßig beschleunigte Bewegung (Danke Mercedes Belz)
-		geschwindigkeitsVektor.vektorAddieren(new Punkt(true,0.3,neigung*Math.PI/180));
+		//gleichmäßig beschleunigte Bewegung (Danke Mercedes Belz)
+		geschwindigkeitsVektor.vektorAddieren(new Punkt(true,beschleunigungsRate,neigung*Math.PI/180));
 	}
 	
 	public void steuern(int richtung) {
-		neigung += 4*richtung;//Richtung kann 1 oder -1 sein
+		neigung += neigungsrate*richtung;//Richtung kann 1 oder -1 sein
 //		Neigung auf 0 bis 360 begrenzen (gut für das HUD)
 		if(neigung > 360) {
 			neigung -= 360;
@@ -32,23 +33,21 @@ public class Rakete {
 	
 	public void positionUpdate() {
 		geschwindigkeitsVektor.vektorAddieren(gravitation);
-		
 		if(stopp) {
 			geschwindigkeitsVektor = new Punkt(0,0);
 		}
-		
 		position.vektorAddieren(geschwindigkeitsVektor);
 	}
 	
 	public void seitenAustritt(int randLinks, int randRechts) {
 		//Austritt Links
 		boolean links = false;
-		if(position.getX()+50 < randLinks) {
+		if(getMittelpunkt().getX() < randLinks) {
 			links = true;
 		}
 		//Austritt rechts
 		boolean rechts = false;
-		if(position.getX()+50 > randRechts) {
+		if(getMittelpunkt().getX() > randRechts) {
 			rechts = true;
 		}
 		//Korrektur
@@ -59,13 +58,6 @@ public class Rakete {
 			position.setX(randLinks-50);
 		}
 	}
-
-//	public void bodenBeruehrung() {
-//		stopp= false;
-//		if(position.getY()+100 > 108*8*0.95) {
-//			stopp = true;
-//		}		
-//	}
 	
 	public Punkt getPosition() {
 		return position;
--- src/Steuerung.java
+++ src/Steuerung.java
@@ -13,7 +13,7 @@ public class Steuerung implements Runnable{
 	}
 	
 	public Steuerung() {
-		dasSpielfeld = new Spielfeld(0.1,108*8);		
+		dasSpielfeld = new Spielfeld(0.05,108*8);//Wird später ein Feld, wenn Mars und Erde kommen
 		spielfeldErstellen();
 		dieRakete = new Rakete(dasSpielfeld.getGravitation());
 		dieGUI = new GUI(this);
@@ -76,34 +76,34 @@ public class Steuerung implements Runnable{
 	private void bodenBeruehrung() {
 		boolean bodenBeruehrt = false;
 		Punkt[] punkt = dasSpielfeld.getDerPunkt();
-		Punkt[] hitPunkte = new Punkt[11];
+		Punkt[] hitPunkt = new Punkt[11];
 		//Punkt S an der Spitze:
-		Punkt s = hitPunktBerechnen(new Punkt(dieRakete.getMittelpunkt()), 50, Math.toRadians(dieRakete.getNeigung()));
+		hitPunkt[0] = hitPunktBerechnen(new Punkt(dieRakete.getMittelpunkt()), 50, Math.toRadians(dieRakete.getNeigung()));
 
 		//Punkte oL, oR
 		Punkt o = hitPunktBerechnen(new Punkt(dieRakete.getMittelpunkt()), -27, Math.toRadians(dieRakete.getNeigung()-180));
-		Punkt oL = hitPunktBerechnen(new Punkt(o), 20, Math.toRadians(dieRakete.getNeigung()-90));
-		Punkt oR = hitPunktBerechnen(new Punkt(o), 20, Math.toRadians(dieRakete.getNeigung()+90));
+		hitPunkt[1] = hitPunktBerechnen(new Punkt(o), 20, Math.toRadians(dieRakete.getNeigung()-90));
+		hitPunkt[2] = hitPunktBerechnen(new Punkt(o), 20, Math.toRadians(dieRakete.getNeigung()+90));
 		
 		//Punkte aL, aR
 		Punkt a = hitPunktBerechnen(new Punkt(dieRakete.getMittelpunkt()), -10, Math.toRadians(dieRakete.getNeigung()-180));
-		Punkt aL = hitPunktBerechnen(new Punkt(a), 20, Math.toRadians(dieRakete.getNeigung()-90));
-		Punkt aR = hitPunktBerechnen(new Punkt(a), 20, Math.toRadians(dieRakete.getNeigung()+90));
+		hitPunkt[3] = hitPunktBerechnen(new Punkt(a), 20, Math.toRadians(dieRakete.getNeigung()-90));
+		hitPunkt[4] = hitPunktBerechnen(new Punkt(a), 20, Math.toRadians(dieRakete.getNeigung()+90));
 		
 		//Punkte bL, bR
 		Punkt b = hitPunktBerechnen(new Punkt(dieRakete.getMittelpunkt()), 8, Math.toRadians(dieRakete.getNeigung()-180));
-		Punkt bL = hitPunktBerechnen(new Punkt(b), 25, Math.toRadians(dieRakete.getNeigung()-90));
-		Punkt bR = hitPunktBerechnen(new Punkt(b), 25, Math.toRadians(dieRakete.getNeigung()+90));
+		hitPunkt[5] = hitPunktBerechnen(new Punkt(b), 25, Math.toRadians(dieRakete.getNeigung()-90));
+		hitPunkt[6] = hitPunktBerechnen(new Punkt(b), 25, Math.toRadians(dieRakete.getNeigung()+90));
 		
 		//Punkte fl, fR
 		Punkt f = hitPunktBerechnen(new Punkt(dieRakete.getMittelpunkt()), 33, Math.toRadians(dieRakete.getNeigung()-180));
-		Punkt fL = hitPunktBerechnen(new Punkt(f), 30, Math.toRadians(dieRakete.getNeigung()-90));
-		Punkt fR = hitPunktBerechnen(new Punkt(f), 30, Math.toRadians(dieRakete.getNeigung()+90));
+		hitPunkt[7] = hitPunktBerechnen(new Punkt(f), 30, Math.toRadians(dieRakete.getNeigung()-90));
+		hitPunkt[8] = hitPunktBerechnen(new Punkt(f), 30, Math.toRadians(dieRakete.getNeigung()+90));
 		
 		//Punkte uL, uR
 		Punkt u = hitPunktBerechnen(new Punkt(dieRakete.getMittelpunkt()), 50, Math.toRadians(dieRakete.getNeigung()-180));
-		Punkt uL = hitPunktBerechnen(new Punkt(u), 20, Math.toRadians(dieRakete.getNeigung()-90));
-		Punkt uR = hitPunktBerechnen(new Punkt(u), 20, Math.toRadians(dieRakete.getNeigung()+90));
+		hitPunkt[9] = hitPunktBerechnen(new Punkt(u), 20, Math.toRadians(dieRakete.getNeigung()-90));
+		hitPunkt[10] = hitPunktBerechnen(new Punkt(u), 20, Math.toRadians(dieRakete.getNeigung()+90));
 
 		for(int i=0; i < dasSpielfeld.getAnzahlPunkte()-1; i++) {
 			double x1 = punkt[i].getX();
@@ -114,68 +114,31 @@ public class Steuerung implements Runnable{
 			double c = y1 - m * x1;	//I  'y' = 'm'*'x'+c    bei ' ' ist Wert bekannt
 									//Ia c = 'y1' - 'm'*'x1'
 			
-						
-//			-------------Kollision mit Unterseite als Gerade--------
-//			Erst gucken ob es sich schneidet -> duh 2D schneidet sich immer...
-//			Schnittpunkt errechnen
-//			x vom Schnittpunkt in Intervall x1 x2?
-//			...
-//			Profit
-//			-----------------------------------------------------
-//			Gerade gU aufstellen
-			double xL = uL.getX();
-			double yL = uL.getY();
-			double xR = uR.getX();
-			double yR = uR.getY();
+			//Kollision mit Unterseite als Gerade
+			//hitPunkt 9 und 10 sind uL und uR
+			double xL = hitPunkt[9].getX();
+			double yL = hitPunkt[9].getY();
+			double xR = hitPunkt[10].getX();
+			double yR = hitPunkt[10].getY();
+			//Ist die Unterseite im Intervall x1 x2?
 			if((x1 <= xL && xR <= x2)||(x1 <= xR && xL <= x2)) {
+				//Geradensteigung m und Y-Achsenabschnitt c der Unterseite ermitteln
 				double mU = (yR-yL)/(xR-xL);
 				double cU = yR - mU * xR;
-				//Schnittpunkt berechnen von Spielfeld Gerade und gU
+				//Schnittstelle berechnen von Spielfeld-Gerade und gU
 				double xS = (cU-c)/(m-mU);
-				if(
-					((xL <= xS && xS <= xR)||(xR <= xS && xS <= xL))&&
-					(x1 <= xS && xS <= x2)
-				){
+				//Ist die Schnittstelle im x Intervall der Gerade des Spielfelds und Unterseite?
+				if(((xL <= xS && xS <= xR)||(xR <= xS && xS <= xL))&&(x1 <= xS && xS <= x2)){
 					bodenBeruehrt = true;
 				}
 			}
+			
 			//Wenn mindestens einer der Kollisionspunkte auf der Geraden liegt dann Stopp
-			if(punktAufGerade(uL, c, x1, x2, m)) {
-				bodenBeruehrt = true;
-			};
-			if(punktAufGerade(uR, c, x1, x2, m)) {
-				bodenBeruehrt = true;
-			};
-			if(punktAufGerade(s, c, x1, x2, m)) {
-				bodenBeruehrt = true;
-			};
-			if(punktAufGerade(fL, c, x1, x2, m)) {
-				bodenBeruehrt = true;
-			};
-			if(punktAufGerade(fR, c, x1, x2, m)) {
-				bodenBeruehrt = true;
-			};
-			if(punktAufGerade(oL, c, x1, x2, m)) {
-				bodenBeruehrt = true;
-			};
-			if(punktAufGerade(oR, c, x1, x2, m)) {
-				bodenBeruehrt = true;
-			};
-			if(punktAufGerade(u, c, x1, x2, m)) {
-				bodenBeruehrt = true;
-			};
-			if(punktAufGerade(aL, c, x1, x2, m)) {
-				bodenBeruehrt = true;
-			};
-			if(punktAufGerade(aR, c, x1, x2, m)) {
-				bodenBeruehrt = true;
-			};
-			if(punktAufGerade(bL, c, x1, x2, m)) {
-				bodenBeruehrt = true;
-			};
-			if(punktAufGerade(bR, c, x1, x2, m)) {
-				bodenBeruehrt = true;
-			};
+			for(int j = 0; j < 11; j++) {
+				if(punktAufGerade(hitPunkt[j], c, x1, x2, m)) {
+					bodenBeruehrt = true;
+				};
+			}
 		}
 		//Später wenn der Boden berürht wird, gibt es keine Steuerung
 		//-> Position wird gesetzt so dass es präzise aussieht auch wenn die
@@ -183,11 +146,85 @@ public class Steuerung implements Runnable{
 		dieRakete.setStopp(bodenBeruehrt);
 	}
 	
+	public void grafik() {
+		//Hintergrund löschen
+		dieGUI.hintergrundLoeschen();
+		//Spielfeld zeichnen
+		dieGUI.spielfeldZeichnen(dasSpielfeld.getAnzahlPunkte(),dasSpielfeld.getDerPunkt());
+		//Rakete zeichnen
+		dieGUI.raketeZeichnen(dieRakete.getPosition(), dieRakete.getMittelpunkt(), dieRakete.getNeigung());
+	}
+
+	private void spielfeldErstellen() {
+		dasSpielfeld.setAnzahlPunkte(30);
+		int breite = dasSpielfeld.getBreite();
+		int hoehe = dasSpielfeld.getHoehe();
+		//Alle Punkte von Hand eintragen
+		dasSpielfeld.setPunkt(0, new Punkt(0,hoehe*0.55));
+		dasSpielfeld.setPunkt(1, new Punkt(breite*0.07,hoehe*0.4));
+		dasSpielfeld.setPunkt(2, new Punkt(breite*0.08,hoehe*0.42));
+		dasSpielfeld.setPunkt(3, new Punkt(breite*0.15,hoehe*0.27));
+		dasSpielfeld.setPunkt(4, new Punkt(breite*0.19,hoehe*0.5));
+		dasSpielfeld.setPunkt(5, new Punkt(breite*0.2,hoehe*0.48));
+		dasSpielfeld.setPunkt(6, new Punkt(breite*0.23,hoehe*0.75));
+		dasSpielfeld.setPunkt(7, new Punkt(breite*0.27,hoehe*0.58));
+		dasSpielfeld.setPunkt(8, new Punkt(breite*0.3,hoehe*0.58));
+		dasSpielfeld.setPunkt(9, new Punkt(breite*0.33,hoehe*0.65));
+		dasSpielfeld.setPunkt(10, new Punkt(breite*0.4,hoehe*0.62));
+		dasSpielfeld.setPunkt(11, new Punkt(breite*0.42,hoehe*0.64));
+		dasSpielfeld.setPunkt(12, new Punkt(breite*0.46,hoehe*0.52));
+		dasSpielfeld.setPunkt(13, new Punkt(breite*0.5,hoehe*0.77));
+		dasSpielfeld.setPunkt(14, new Punkt(breite*0.51,hoehe*0.75));
+		dasSpielfeld.setPunkt(15, new Punkt(breite*0.53,hoehe*0.85));
+		dasSpielfeld.setPunkt(16, new Punkt(breite*0.55,hoehe*0.83));
+		dasSpielfeld.setPunkt(17, new Punkt(breite*0.59,hoehe*0.87));
+		dasSpielfeld.setPunkt(18, new Punkt(breite*0.7,hoehe*0.87));
+		dasSpielfeld.setPunkt(19, new Punkt(breite*0.72,hoehe*0.69));
+		dasSpielfeld.setPunkt(20, new Punkt(breite*0.73,hoehe*0.71));
+		dasSpielfeld.setPunkt(21, new Punkt(breite*0.76,hoehe*0.64));
+		dasSpielfeld.setPunkt(22, new Punkt(breite*0.81,hoehe*0.75));
+		dasSpielfeld.setPunkt(23, new Punkt(breite*0.84,hoehe*0.72));
+		dasSpielfeld.setPunkt(24, new Punkt(breite*0.87,hoehe*0.72));
+		dasSpielfeld.setPunkt(25, new Punkt(breite*0.89,hoehe*0.68));
+		dasSpielfeld.setPunkt(26, new Punkt(breite*0.92,hoehe*0.72));
+		dasSpielfeld.setPunkt(27, new Punkt(breite*0.94,hoehe*0.65));
+		dasSpielfeld.setPunkt(28, new Punkt(breite*0.95,hoehe*0.67));
+		dasSpielfeld.setPunkt(29, new Punkt(breite,hoehe*0.55));
+		
+//		für Test zwecke random Map
+//		for(int i = 1; i < dasSpielfeld.getAnzahlPunkte()-1; i++) {
+//			int x = dasSpielfeld.getBreite() / (dasSpielfeld.getAnzahlPunkte()-1) * i;
+//			int y = (int)((0.95*dasSpielfeld.getHoehe())-Math.random()*(dasSpielfeld.getHoehe()*0.75));
+//			Punkt p = new Punkt(x,y);
+//			dasSpielfeld.setPunkt(i, p);
+//		}
+	}
+
+	@Override
+	public void run(){
+		double timePerTick = 1000000000 / 60;
+	    double delta = 0;
+	    long now;
+	    long lastTime = System.nanoTime();
+		while(true) {
+			now = System.nanoTime();
+			delta += (now - lastTime) / timePerTick;
+		   	lastTime = now;
+		   	if(delta >= 1) {
+		    //Spielogik
+		    	//Aktualisieren
+		    	update();
+		    	//Zeichnen
+		    	dieGUI.repaint();
+		    	delta--;
+		    }
+		}
+	}
+	
 	private boolean punktAufGerade(Punkt p, double c, double x1, double x2, double m) {
 		boolean istAufGerade = false;
 		if((x1 <= p.getX())&&(p.getX() <= x2)){
-			double cP = p.getY()-m*
-						p.getX();
+			double cP = p.getY()-m*p.getX();
 			if(c <= cP) {
 				istAufGerade = true;
 			}
@@ -200,68 +237,6 @@ public class Steuerung implements Runnable{
 		Punkt vektor = new Punkt(true, abstand, winkel);
 		hitPunkt.vektorAddieren(vektor);
 		return hitPunkt;
-	}
-	
-	public void grafik() {
-		//Hintergrund löschen
-		dieGUI.hintergrundLoeschen();
-		//Spielfeld zeichnen
-		dieGUI.spielfeldZeichnen(dasSpielfeld.getAnzahlPunkte(),dasSpielfeld.getDerPunkt());
-		//Rakete zeichnen
-		dieGUI.raketeZeichnen(dieRakete.getPosition(), dieRakete.getMittelpunkt(), dieRakete.getNeigung());
-	}
-	
-	
-	private void spielfeldErstellen() {
-		dasSpielfeld.setAnzahlPunkte(20);
-		
-		//Erster Punkt immer gleich
-		dasSpielfeld.setPunkt(0, new Punkt(0,dasSpielfeld.getHoehe()*0.8));
-		
-		//Der Rest
-		for(int i = 1; i < dasSpielfeld.getAnzahlPunkte()-1; i++) {
-			int x = dasSpielfeld.getBreite() /
-					(dasSpielfeld.getAnzahlPunkte()-1) * i;
-			int y = (int)(
-				(0.95*dasSpielfeld.getHoehe())-
-				Math.random()*(dasSpielfeld.getHoehe()*0.75)
-			);
-			Punkt p = new Punkt(x,y);
-			dasSpielfeld.setPunkt(i, p);
-		}
-		
-		
-		//Letzter Punkt immer gleich
-		dasSpielfeld.setPunkt(
-			dasSpielfeld.getAnzahlPunkte()-1,
-			new Punkt(dasSpielfeld.getBreite(), dasSpielfeld.getHoehe()*0.8)
-		);
-	}
-	
-	
-	@Override
-	public void run(){
-		double timePerTick = 1000000000 / 60;
-	    double delta = 0;
-	    long now;
-	    long lastTime = System.nanoTime();
-		while(true) {
-			now = System.nanoTime();
-			delta += (now - lastTime) / timePerTick;
-		   	lastTime = now;
-
-		    if (delta >= 1) {
-		    
-		    //Spielogik
-		    	
-		    	//Aktualisieren
-		    	update();
-			
-		    	//Zeichnen
-		    	dieGUI.repaint();
-		    	delta--;
-		    }
-		}
 	}
 
 	public Spielfeld getDasSpielfeld() {
